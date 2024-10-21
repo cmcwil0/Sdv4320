52ebe15b8e6b53b8d62bb52cb691fa93d2af487c
lukefisha <lafbbz@umsystem.edu>
2024-10-21 12:15:14 -0500

    Edited ui prompts to obtain correct information from user

--- StockDataVisualizer.py
+++ StockDataVisualizer.py
@@ -7,20 +7,24 @@ print('-------Stock Data Visualizer-------')
 print('Enter the stock symbol you are looking for: ')
 stockSymbol = input()
 
-
-print('Enter the chart type you want (1,2): ')
+print('''
+Chart Types
+-----------
+1. Bar
+2. Line
+      
+Enter the chart type you want (1,2): ''')
 chartType = input()
 
 print('''
 Select the Time Series of the chart you want to Generate
 --------------------------------------------------------
-1.
-2.
-3.
-4.
+1. Intraday
+2. Daily
+3. Weekly
+4. Monthly
 
-Enter time series option (1, 2, 3, 4):
-''')
+Enter time series option (1, 2, 3, 4):''')
 timeSeries = input()
 
 print('Enter the start Date (YYYY-MM-DD): ')
@@ -33,3 +37,7 @@ endDate = input()
 #Chart opens here
 
 
+#after chart opens
+
+print('Would you like to view more stock data? Press y to continue:')
+viewMore = input()
